f8954c17d0789f6db15a041c9a7822b4ef2f2b84
Hans Vanselow-Rasmussen <angdeclo@gmail.com>
2019-02-01 17:03:40 +0100

    Made it easier to setup the environment

---
+++ C++_Arduino_combined/PCD/setup.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+# Steps to be taken to setup the environment:
+
+# Install python-serial (and git)
+sudo apt update
+sudo apt install python-serial git
+
+# Clone the arduino-mk project by sudar into the PCD directory
+git clone https://github.com/sudar/Arduino-Makefile.git
+
+#Download and extract arduino into the PCD directory
+mkdir tmp && cd tmp
+wget https://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
+tar xf arduino-1.8.8-linux64.tar.xz -C ..
+
+# Download and add the needed libraries:
+wget http://arduiniana.org/Streaming/Streaming5.zip
+tar xf Streamin5.zip -C ../arduino-1.8.8/libraries/
+cd ../arduino-1.8.8/libraries/
+git clone https://github.com/JChristensen/DS3232RTC.git
+git clone https://github.com/PaulStoffregen/Time.git
+mv ./Time ./TimeLib
+
+
+
+# Optional: add youself to the dialout group and restart computer (so you don't need to run make as sudo):
+#sudo usermod -a -G dialout <username>
